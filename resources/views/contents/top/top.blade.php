7c89642849af524b95d390a8bf49da6bfc7c5b57
masa-t <halfmoonra@gmail.com>
2018-05-06 16:23:08 +0900

    [update]更新履歴をパーツ化

--- resources/views/contents/top/top.blade.php
+++ resources/views/contents/top/top.blade.php
@@ -7,9 +7,7 @@
         <div class="row">
             <div class="col-lg-10 col-md-10 mx-auto">
                 <p>更新履歴</p>
-                <hr>
-                2018/05/06:Webサイト開設
-                <hr>
+                @include('contents.top.parts.updateLog')
             </div>
         </div>
         <div class="row">
